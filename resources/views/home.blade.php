82b756fb551386ef389230e486a6289895053034
ibuki0624 <ibu0624.203@gmail.com>
2024-08-21 12:36:33 +0900

    ホーム

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            トップページ
+            ホーム
         </h2>
     </x-slot>
 
@@ -10,9 +10,20 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <a href="{{ route('post.index') }}" class="bg-white border-b border-gray-200 p-6 block w-full text-center
                 font-semibold text-gray-800 hover:bg-gray-100 text-decoration-none">
-                    掲示板へ移動
+                    AIに質問
                 </a>
             </div>
         </div>
     </div>
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <a href="{{ route('post.index') }}" class="bg-white border-b border-gray-200 p-6 block w-full text-center
+                font-semibold text-gray-800 hover:bg-gray-100 text-decoration-none">
+                    登録
+                </a>
+            </div>
+        </div>
+    </div>
+
 </x-app-layout>
